8a9102e26a9d4384dadfed6105354e21a04a850f
Igor Mandrigin <i@mandrigin.ru>
2020-09-22 18:18:20 +0200

    simpler logs

--- cmd/supply/supply.go
+++ cmd/supply/supply.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/binary"
-	"fmt"
 	"math/big"
 	"time"
 
@@ -33,8 +32,7 @@ func calculateEthSupply(db ethdb.Database, from, currentStateAt uint64) error {
 
 	p := message.NewPrinter(language.English)
 
-	var previousLog *time.Time
-	var previousBlockNumber uint64
+	previousLog := time.Now()
 
 	for blockNumber >= from {
 		supply := big.NewInt(0)
@@ -93,34 +91,12 @@ func calculateEthSupply(db ethdb.Database, from, currentStateAt uint64) error {
 		}
 		count = len(balances)
 
-		printLog := false
-		speed := 0.0
-		if previousLog == nil {
-			now := time.Now()
-			previousLog = &now
-			printLog = true
-			previousBlockNumber = blockNumber
-		} else {
-			now := time.Now()
-			timeSpent := now.Sub(*previousLog)
-			if timeSpent > 10*time.Second {
-				printLog = true
-				speed = float64(previousBlockNumber-blockNumber) / float64(timeSpent)
-				previousBlockNumber = blockNumber
-				previousLog = &now
-			}
-		}
+		now := time.Now()
+		timeSpent := now.Sub(previousLog)
+		blocksLeft := blockNumber - from
+		timeLeft := time.Duration(blocksLeft) * timeSpent
 
-		if printLog {
-			left := "unknown"
-			if speed > 0.00000001 {
-				left = fmt.Sprintf("%v", time.Duration(float64(blockNumber-from)/speed)*time.Second)
-			}
-			log.Info(p.Sprintf("Block %d, total accounts: %d, supply: %d, speed %.2f blocks/sec left=%v\n", blockNumber, count, supply, speed, left))
-			if speed < 0.000000001 { // first launch is 0.0, but with floats you are never sure
-				log.Info("Will calculate supply for historical blocks now")
-			}
-		}
+		log.Info(p.Sprintf("blockNum=%d, total accounts=%d, supply=%d, timePerBlock=%v timeLeft=%v\n", blockNumber, count, supply, timeSpent, timeLeft))
 
 		if err := db.Put(ethSupplyBucket, keyFromBlockNumber(blockNumber), supply.Bytes()); err != nil {
 			return err
