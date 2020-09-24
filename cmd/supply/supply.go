6ec459c1f3e268bb3c72557cc7fe4e5bfc8e8e36
Igor Mandrigin <i@mandrigin.ru>
2020-09-24 17:53:37 +0200

    code cleanup

--- cmd/supply/supply.go
+++ cmd/supply/supply.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 	"math/big"
-	"os"
 
 	"github.com/holiman/uint256"
 	"github.com/ledgerwatch/turbo-geth/common"
@@ -32,40 +30,33 @@ func calculateEthSupply(db ethdb.Database, from, currentStateAt uint64) error {
 
 	log.Info("computing eth supply", "from", from, "to", currentStateAt)
 
-	balances := make(map[[20]byte]*uint256.Int)
+	accountBalances := make(map[common.Address]*uint256.Int)
 
 	p := message.NewPrinter(language.English)
 
-	supply := uint256.NewInt()
+	totalSupply := uint256.NewInt()
 
-	changedAccounts := 0
 	for blockNumber >= from {
-		count := 0
-
-		totalRemove.Clear()
-		totalRemoveAccount.Clear()
-		totalAdd.Clear()
-		totalCreated.Clear()
 
 		if blockNumber == currentStateAt {
 			log.Info("Calculating supply for the current state (will be slow)")
+			processed := 0
 			err := db.Walk(dbutils.PlainStateBucket, nil, 0, func(k, v []byte) (bool, error) {
 				if !isAccount(k) {
 					// for storage entries we just continue
 					return true, nil
 				}
 
-				var kk [20]byte
-				copy(kk[:], k)
+				address := common.BytesToAddress(k)
 
-				err := DecodeAccountRLP(v, kk, balances, supply, false)
+				err := decodeAccountAndUpdateBalance(v, address, accountBalances, totalSupply)
 				if err != nil {
 					return false, err
 				}
 
-				count++
-				if count%100000 == 0 {
-					log.Info(p.Sprintf("Processed %d account records in current state\n", count))
+				processed++
+				if processed%100000 == 0 {
+					log.Info(p.Sprintf("Processed %d account records in current state\n", processed))
 				}
 
 				return true, nil
@@ -74,122 +65,61 @@ func calculateEthSupply(db ethdb.Database, from, currentStateAt uint64) error {
 				return err
 			}
 		} else {
-			changeSet, err := db.Get(dbutils.PlainAccountChangeSetBucket, dbutils.EncodeTimestamp(blockNumber))
+			// to get the state for blockNumber if we have the state for blockNuber + 1
+			// we need to apply changesets by key blockNumber + 1 to the state
+			changesetKey := dbutils.EncodeTimestamp(blockNumber + 1)
+			changeSet, err := db.Get(dbutils.PlainAccountChangeSetBucket, changesetKey)
 			if err != nil {
 				return err
 			}
-			changedAccounts = 0
 			err = changeset.AccountChangeSetPlainBytes(changeSet).Walk(func(k, v []byte) error {
-				trace := isInterestingAccount(k)
-
-				var kk [20]byte
-				copy(kk[:], k)
-
-				if trace {
-					fmt.Printf("BEGIN_TRACE: block=%d k=%x kk=%x idx=%d\n", blockNumber, k, kk, changedAccounts)
-				}
-				defer func() {
-					if trace {
-						fmt.Printf("END_TRACE: block=%d k=%x kk=%x idx=%d\n", blockNumber, k, kk, changedAccounts)
-					}
-				}()
-
-				changedAccounts++
-
-				return DecodeAccountRLP(v, kk, balances, supply, trace)
+				address := common.BytesToAddress(k)
+				return decodeAccountAndUpdateBalance(v, address, accountBalances, totalSupply)
 			})
 			if err != nil {
 				return err
 			}
 		}
 
-		count = len(balances)
+		ethHoldersCount := len(accountBalances) // those who have non-zero balance
 
 		if blockNumber%10_000 == 0 {
-			fmt.Printf("progress. block=%v\n", blockNumber)
+			log.Info(p.Sprintf("Stats: blockNum=%d\n\ttotal accounts with non zero balance=%d\n\tsupply=%d", blockNumber, ethHoldersCount, totalSupply))
 		}
 
-		if blockNumber%1_000_000 == 0 {
-			log.Info(p.Sprintf("Stats: blockNum=%d\n\ttotal accounts with non zero balance=%d\n\tsupply=%d", blockNumber, count, supply))
-		}
-
-		for _, acc := range accountsToTrace {
-			if _, ok := disappearedAccounts[acc]; !ok {
-				addr := common.HexToAddress(fmt.Sprintf("0x%s", acc))
-				if _, ok := balances[addr]; !ok {
-					fmt.Printf("Account %s disappered in the block %d\n", acc, blockNumber)
-					disappearedAccounts[acc] = struct{}{}
-				}
-			}
+		if err := db.Put(ethSupplyBucket, keyFromBlockNumber(blockNumber), totalSupply.Bytes()); err != nil {
+			return err
 		}
 
 		if blockNumber == 0 {
-			for k, b := range balances {
-				fmt.Printf("Accounts in Genesis %x %v\n", k, b)
-			}
+			break
 		}
-
-		/*
-			if err := db.Put(ethSupplyBucket, keyFromBlockNumber(blockNumber), supply.Bytes()); err != nil {
-				return err
-			}
-		*/
 
 		blockNumber--
 	}
-
-	os.Exit(0) // we are done and I don't want the sync to continue
-	panic("boomio")
 }
-
-var accountsToTrace = []string{
-	"e83604E4fF6Be7f96F6018D3eC3072ec525DFF6b",
-	"5c6F36AF90AB1A656c6ec8c7d521512762bba3E1",
-	"d6a7Ac4De7b510F0e8De519d973fA4C01Ba83400",
-}
-
-var disappearedAccounts = make(map[string]struct{})
-
-func isInterestingAccount(k []byte) bool {
-	for _, acc := range accountsToTrace {
-		addr := common.HexToAddress(fmt.Sprintf("0x%s", acc))
-		if bytes.Equal(addr[:], k) {
-			return true
-		}
-	}
-	return false
-}
-
-var totalRemove *uint256.Int = uint256.NewInt()
-var totalAdd *uint256.Int = uint256.NewInt()
-var totalCreated *uint256.Int = uint256.NewInt()
-var totalRemoveAccount *uint256.Int = uint256.NewInt()
 
 // inspired by accounts.Account#DecodeForStorage, but way more light weight
-func DecodeAccountRLP(enc []byte, kk [20]byte, balances map[[20]byte]*uint256.Int, supply *uint256.Int, trace bool) error {
+// it uses some knowledge about how turbo-geth stores accounts
+// but it makes the operations with very good performance
+func decodeAccountAndUpdateBalance(enc []byte, address common.Address, balances map[common.Address]*uint256.Int, totalSupply *uint256.Int) error {
+	// if an account was removed
 	if len(enc) == 0 {
-		if balance, ok := balances[kk]; ok && balance != nil {
-			if trace {
-				fmt.Printf("delete: %v -> 0 (delete acc)\n", balance)
-			}
-			supply.Sub(supply, balance)
-			totalRemoveAccount.Add(totalRemoveAccount, balance)
-		}
-		delete(balances, kk)
+		// remove it from the list of balances
+		delete(balances, address)
 		return nil
 	}
 
 	var fieldSet = enc[0]
 
-	if fieldSet&2 <= 0 { // no balance to check
-		if balance, ok := balances[kk]; ok && balance != nil {
-			if trace {
-				fmt.Printf("update: %v -> 0 (delete acc)\n", balance)
-			}
-			supply.Sub(supply, balance)
-			totalRemove.Add(totalRemoveAccount, balance)
+	// if an account has 0 balance...
+	if fieldSet&2 <= 0 {
+		// ...and it had some balance before
+		if balance, ok := balances[address]; ok && balance != nil {
+			// set it to 0 and update the value
+			balance.Clear()
+			balances[address] = balance
 		}
-		delete(balances, kk)
 		return nil
 	}
 
@@ -207,54 +137,35 @@ func DecodeAccountRLP(enc []byte, kk [20]byte, balances map[[20]byte]*uint256.In
 		pos += decodeLength + 1
 	}
 
-	if fieldSet&2 > 0 {
-		decodeLength := int(enc[pos])
+	decodeLength := int(enc[pos])
 
-		if len(enc) < pos+decodeLength+1 {
-			return fmt.Errorf(
-				"malformed CBOR for Account.Nonce: %s, Length %d",
-				enc[pos+1:], decodeLength)
-		}
-
-		/*
-			if decodeLength == 0 {
-				if balance, ok := balances[kk]; ok && balance != nil {
-					//fmt.Printf("update %v -> 0\n", balance)
-					supply.Sub(supply, balance)
-					balance.Clear()
-					balances[kk] = balance
-				}
-				return nil
-			}
-		*/
-
-		if oldBalance, ok := balances[kk]; ok && oldBalance != nil {
-			if trace {
-				fmt.Printf("update: %v ->", oldBalance)
-			}
-			supply.Sub(supply, oldBalance)
-			totalRemove.Add(totalRemove, oldBalance)
-			oldBalance.SetBytes(enc[pos+1 : pos+decodeLength+1])
-			supply.Add(supply, oldBalance)
-			totalAdd.Add(totalAdd, oldBalance)
-			oldBalance.SetBytes(enc[pos+1 : pos+decodeLength+1])
-			if trace {
-				fmt.Printf("%v\n", oldBalance)
-			}
-			balances[kk] = oldBalance
-		} else {
-			balance := uint256.NewInt().SetBytes(enc[pos+1 : pos+decodeLength+1])
-			if trace {
-				fmt.Printf("create: 0 -> %v\n", balance)
-			}
-			supply.Add(supply, balance)
-			totalCreated.Add(totalAdd, balance)
-			balances[kk] = balance
-		}
+	if len(enc) < pos+decodeLength+1 {
+		return fmt.Errorf(
+			"malformed CBOR for Account.Nonce: %s, Length %d",
+			enc[pos+1:], decodeLength)
 	}
 
-	// we theoretically should never get there
-	return nil
+	// update existing balance if we found it
+	if balance, ok := balances[address]; ok && balance != nil {
+		// remove the old value
+		totalSupply.Sub(totalSupply, balance)
+
+		// update value in-place so we don't overload the GC
+		balance.SetBytes(enc[pos+1 : pos+decodeLength+1])
+
+		// add the new value
+		totalSupply.Add(totalSupply, balance)
+
+		// and update the map
+		balances[address] = balance
+		return nil
+	}
+
+	// add a new entry to if there wasn't an existing one
+	balance := uint256.NewInt().SetBytes(enc[pos+1 : pos+decodeLength+1])
+	balances[address] = balance
+
+	totalSupply.Add(totalSupply, balance)
 }
 
 func unwindEthSupply(db ethdb.Database, from, to uint64) (err error) {
