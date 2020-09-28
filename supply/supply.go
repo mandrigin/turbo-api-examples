7ac766de179466babc89a43b1884ea3dae028ed2
Igor Mandrigin <i@mandrigin.ru>
2020-09-28 11:13:07 +0200

    simple stuff

--- supply/supply.go
+++ supply/supply.go
@@ -6,6 +6,7 @@ import (
 	"math/big"
 
 	"github.com/holiman/uint256"
+	"github.com/ledgerwatch/turbo-geth/common/changeset"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
 	"github.com/ledgerwatch/turbo-geth/core"
 	"github.com/ledgerwatch/turbo-geth/core/state"
@@ -34,121 +35,94 @@ func Calculate(db ethdb.Database, from, currentStateAt uint64) error {
 
 	for blockNumber := from; blockNumber < currentStateAt; blockNumber++ {
 		var err error
-		var ethHoldersCount int
-		if blockNumber == currentStateAt {
-			log.Warn("calculating supply at the tip is not supported yet")
-		} else if blockNumber == 0 {
+		if blockNumber == 0 {
 			// calc from genesis
-			ethHoldersCount, err = calculateAtGenesis(db, totalSupply)
-			log.Info(p.Sprintf("Stats: blockNum=%d\n\ttotal accounts with non zero balance=%d\n\tsupply=%d", blockNumber, ethHoldersCount, totalSupply))
+			err = calculateAtGenesis(db, totalSupply)
 		} else {
-			panic("boom")
+			err = calculateAtBlock(db, blockNumber, totalSupply)
 		}
 
 		if err != nil {
 			return err
 		}
+
+		log.Info(p.Sprintf("Stats: blockNum=%d\n\tsupply=%d", blockNumber, totalSupply))
 	}
-
-	/*
-		for blockNumber >= from {
-			if blockNumber == currentStateAt {
-				log.Info("Calculating supply for the current state (will be slow)")
-				processed := 0
-				err := db.Walk(dbutils.PlainStateBucket, nil, 0, func(k, v []byte) (bool, error) {
-					if !isAccount(k) {
-						// for storage entries we just continue
-						return true, nil
-					}
-
-					address := common.BytesToAddress(k)
-
-					err := decodeAccountAndUpdateBalance(v, address, accountBalances, totalSupply)
-					if err != nil {
-						return false, err
-					}
-
-					processed++
-					if processed%100000 == 0 {
-						log.Info(p.Sprintf("Processed %d account records in current state\n", processed))
-					}
-
-					return true, nil
-				})
-				if err != nil {
-					return err
-				}
-			} else {
-				// to get the state for blockNumber if we have the state for blockNuber + 1
-				// we need to apply changesets by key blockNumber + 1 to the state
-				changesetKey := dbutils.EncodeTimestamp(blockNumber + 1)
-				changeSet, err := db.Get(dbutils.PlainAccountChangeSetBucket, changesetKey)
-				if err != nil {
-					return err
-				}
-				err = changeset.AccountChangeSetPlainBytes(changeSet).Walk(func(k, v []byte) error {
-					address := common.BytesToAddress(k)
-					return decodeAccountAndUpdateBalance(v, address, accountBalances, totalSupply)
-				})
-				if err != nil {
-					return err
-				}
-			}
-
-			ethHoldersCount := len(accountBalances) // those who have non-zero balance
-
-			if blockNumber%10_000 == 0 {
-				log.Info(p.Sprintf("Stats: blockNum=%d\n\ttotal accounts with non zero balance=%d\n\tsupply=%d", blockNumber, ethHoldersCount, totalSupply))
-			}
-
-			if err := SetSupplyForBlock(db, blockNumber, totalSupply); err != nil {
-				return err
-			}
-
-			if blockNumber == 0 {
-				break
-			}
-
-			blockNumber--
-		}
-	*/
 
 	return nil
 }
 
-func calculateAtGenesis(db ethdb.Database, totalSupply *uint256.Int) (int, error) {
-	var genesis *core.Genesis
+var (
+	oldBalanceBuffer = uint256.NewInt()
+	newBalanceBuffer = uint256.NewInt()
+)
 
-	holderCount := 0
+func calculateAtBlock(db ethdb.Database, blockNumber uint64, totalSupply *uint256.Int) error {
+	changesetKey := dbutils.EncodeTimestamp(blockNumber)
+	changeSet, err := db.Get(dbutils.PlainAccountChangeSetBucket, changesetKey)
+	if err != nil {
+		return err
+	}
+	err = changeset.AccountChangeSetPlainBytes(changeSet).Walk(func(k, accountDataBeforeBlock []byte) error {
+		err = decodeAccountBalanceTo(accountDataBeforeBlock, oldBalanceBuffer)
+		if err != nil {
+			return err
+		}
+
+		var accountDataAfterBlock []byte
+		accountDataAfterBlock, err = GetAsOf(db, false, k, blockNumber+1)
+		if err != nil {
+			return err
+		}
+
+		err = decodeAccountBalanceTo(accountDataAfterBlock, newBalanceBuffer)
+		if err != nil {
+			return err
+		}
+
+		totalSupply.Sub(totalSupply, oldBalanceBuffer)
+		totalSupply.Add(totalSupply, newBalanceBuffer)
+
+		return nil
+	})
+	return err
+}
+
+func calculateAtGenesis(db ethdb.Database, totalSupply *uint256.Int) error {
+	var genesis *core.Genesis
 
 	block, _, _, err := genesis.ToBlock(nil, false)
 	if err != nil {
-		return 0, err
+		return err
 	}
-
-	zero := uint256.NewInt()
 
 	for _, tx := range block.Transactions() {
 		recipient := tx.To()
-		var accData []byte
-		var err error
-		if txHolder, ok := db.(ethdb.HasTx); ok {
-			accData, err = GetAsOfTx(txHolder.Tx(), false, recipient[:], 1)
-		} else {
-			accData, err = state.GetAsOf(db.(ethdb.KV), false, recipient[:], 1)
-		}
+		accountDataAfterGenesis, err := GetAsOf(db, false, recipient[:], 1)
 		if err != nil {
-			return 0, err
-		}
-		err = decodeAccountAndUpdateBalance(accData, zero, totalSupply)
-		if err != nil {
-			return 0, err
+			return err
 		}
 
-		holderCount++
+		err = decodeAccountBalanceTo(accountDataAfterGenesis, newBalanceBuffer)
+		if err != nil {
+			return err
+		}
+
+		totalSupply.Add(totalSupply, newBalanceBuffer)
 	}
 
-	return holderCount, nil
+	return nil
+}
+
+func GetAsOf(db ethdb.Database, storage bool, key []byte, timestamp uint64) ([]byte, error) {
+	var accData []byte
+	var err error
+	if txHolder, ok := db.(ethdb.HasTx); ok {
+		accData, err = GetAsOfTx(txHolder.Tx(), false, key, 1)
+	} else {
+		accData, err = state.GetAsOf(db.(ethdb.KV), false, key, 1)
+	}
+	return accData, err
 }
 
 func GetAsOfTx(tx ethdb.Tx, storage bool, key []byte, timestamp uint64) ([]byte, error) {
@@ -177,12 +151,9 @@ func GetAsOfTx(tx ethdb.Tx, storage bool, key []byte, timestamp uint64) ([]byte,
 // inspired by accounts.Account#DecodeForStorage, but way more light weight
 // it uses some knowledge about how turbo-geth stores accounts
 // but it makes the operations with very good performance
-func decodeAccountAndUpdateBalance(enc []byte, prevBalance *uint256.Int, totalSupply *uint256.Int) error {
-	// if an account was removed...
+func decodeAccountBalanceTo(enc []byte, to *uint256.Int) error {
+	to.Clear()
 	if len(enc) == 0 {
-		if prevBalance.Uint64() > 0 {
-			totalSupply.Sub(totalSupply, prevBalance)
-		}
 		return nil
 	}
 
@@ -190,11 +161,6 @@ func decodeAccountAndUpdateBalance(enc []byte, prevBalance *uint256.Int, totalSu
 
 	// if an account has 0 balance...
 	if fieldSet&2 <= 0 {
-		// ...and it had some balance before
-		if prevBalance.Uint64() > 0 {
-			// set it to 0 and update the value
-			totalSupply.Sub(totalSupply, prevBalance)
-		}
 		return nil
 	}
 
@@ -220,21 +186,7 @@ func decodeAccountAndUpdateBalance(enc []byte, prevBalance *uint256.Int, totalSu
 			enc[pos+1:], decodeLength)
 	}
 
-	// update existing balance if we found it
-	if prevBalance.Uint64() > 0 {
-		// remove the old value
-		totalSupply.Sub(totalSupply, prevBalance)
-	}
-
-	balance := prevBalance // perf optimization on reuse so we don't alloc unneeded stuff
-	balance.Clear()
-
-	// update value in-place so we don't overload the GC
-	balance.SetBytes(enc[pos+1 : pos+decodeLength+1])
-
-	// add the new value
-	totalSupply.Add(totalSupply, balance)
-
+	to.SetBytes(enc[pos+1 : pos+decodeLength+1])
 	return nil
 }
 
